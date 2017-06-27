2b15a39dfa3fb34ed87cefb04cc45f38e07b1378
ym4199 <ym4199@gmail.com>
2017-06-28 00:18:17 +0900

    sms send 성공

--- send/models.py
+++ send/models.py
@@ -1,3 +1,7 @@
-from django.db import models
-
 # Create your models here.
+#
+# class Sms(models.ModelForm):
+#     send_to = models.CharField(max_length=12)
+#
+#     send_text = models.CharField(max_length=45)
+#     send_date = models.DateTimeField(auto_now_add=True)
--- send/views.py
+++ send/views.py
@@ -1,7 +1,50 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from sdk.api.message import Message
+from sdk.exceptions import CoolsmsException
+
 
 def send_sms(request):
-    return HttpResponse('hi')
+    if request.method == 'POST':
+        info = request.POST
+
+        api_key = "replace value"
+        api_secret = "replace value"
+
+        params = dict()
+        params['type'] = 'sms'
+        params['to'] = info['send_to']
+        params['from'] = '010********'
+        params['text'] = info['send_text']
+
+        cool = Message(api_key, api_secret)
+
+        try:
+            response = cool.send(params)
+            print("Success Count : %s" % response['success_count'])
+            print("Error Count : %s" % response['error_count'])
+            print("Group ID : %s" % response['group_id'])
+
+            if "error_list" in response:
+                print("Error List : %s" % response['error_list'])
+
+            else:
+                context = {
+                    'send_to': params['to'],
+                    'send_text': params['text'],
+                }
+
+                return render(request, 'sms.html', context)
+
+        except CoolsmsException as e:
+            print("Error Code : %s" % e.code)
+            print("Error Message : %s" % e.msg)
+
+
+    else:
+
+        context = {
+            'send_from': '01029953874'
+        }
+
+        return render(request, 'sms.html', context)
